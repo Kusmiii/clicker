5ecd1510af2643c58acd790fcbc146ccd4a13fff
3109676 <3109676@TE-GDA-D-D37H11>
2022-03-23 13:46:13 +0100

    Dodaj pliki projektów.

---
+++ clicker/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.upgradeButton = new System.Windows.Forms.Button();
+            this.buttonLevelTextBox = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(261, 231);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Wal!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(380, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kaska: 0$";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // upgradeButton
+            // 
+            this.upgradeButton.Location = new System.Drawing.Point(142, 286);
+            this.upgradeButton.Name = "upgradeButton";
+            this.upgradeButton.Size = new System.Drawing.Size(133, 49);
+            this.upgradeButton.TabIndex = 2;
+            this.upgradeButton.Text = "Ulepszenie";
+            this.upgradeButton.UseVisualStyleBackColor = true;
+            this.upgradeButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonLevelTextBox
+            // 
+            this.buttonLevelTextBox.AutoSize = true;
+            this.buttonLevelTextBox.Location = new System.Drawing.Point(142, 268);
+            this.buttonLevelTextBox.Name = "buttonLevelTextBox";
+            this.buttonLevelTextBox.Size = new System.Drawing.Size(117, 15);
+            this.buttonLevelTextBox.TabIndex = 3;
+            this.buttonLevelTextBox.Text = "Poziom ulepszenia: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonLevelTextBox);
+            this.Controls.Add(this.upgradeButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Button upgradeButton;
+        private Label buttonLevelTextBox;
+    }
+}
